275029d20be46e5a28ca7ee2c7883b293e15f9c7
Zeno Zeng <zenoofzeng@gmail.com>
2015-06-10 04:25:30 +0800

    add filter

--- static/main.js
+++ static/main.js
@@ -2,11 +2,23 @@ $(function() {
 
     var tags = {
         "Family": ["黑体", "宋体", "楷体", "仿宋", "其他"],
-        "License": ["GPL", "SIL", "文鼎公众授权", "CC", "Apache"],
+        "License": ["GPL", "SIL", "文鼎公众授权", "Creative Commons", "Apache"],
         "Tag": ["可商用", "可嵌入PDF"]
     };
 
-    var $main = $('#main');
+    $('#filter').on('input', function() {
+        var words = $(this).val().split(' ');
+        words = words.filter(function(word) {
+            return word.length > 0;
+        });
+        $('.font').each(function() {
+            var text = $(this)[0].dataset.index.toLowerCase();
+            var display = words.length == 0 || words.every(function(word) {
+                return text.indexOf(word.toLowerCase()) > -1;
+            });
+            $(this).toggle(display);
+        });
+    });
 
     var $tags = $('<div id="tags"></div>');
     Object.keys(tags).forEach(function(key) {
@@ -16,33 +28,63 @@ $(function() {
         });
         $tags.append('<br>');
     });
-    $main.append($tags);
 
-    $('#filter').on('input', function() {
-        var words = $(this).val().split(' ');
-        words = words.filter(function(word) {
-            return word.length > 0;
-        });
-        $('.font').each(function() {
-            var text = $(this).text().toLowerCase();
-            var display = words.length == 0 || words.some(function(word) {
-                return text.indexOf(word.toLowerCase()) > -1;
-            });
-            $(this).toggle(display);
-        });
-    });
+    var $main = $('#main');
+    $main.append($tags);
 
     $('#tags').on('click', '.tag', function() {
         var tag = $(this).text();
         var key = $(this).prevAll('.key').text().split(':')[0];
-        $('#filter').val($('#filter').val() + " " + key + ":" + tag);
+        var currentWords = $('#filter').val();
+        var word = key != "Tag" ? key + ":" + tag : tag;
+        if (currentWords.indexOf(word) > -1) {
+            currentWords = currentWords.replace(word, '');
+        } else {
+            currentWords += " " + word;
+        }
+        $('#filter').val(currentWords.replace(/^ /g, '').replace(/  /g, ' '));
         $('#filter').trigger('input');
     });
 
-
     var fonts = $.get('src/fonts.json', function(fonts) {
+
+        fonts = fonts.map(function(font) {
+            // "Family": ["黑体", "宋体", "楷体", "仿宋", "其他"],
+            var family = {
+                "黑体": [/黑体/, /黑$/, /Hei/],
+                "宋体": [/[^仿]宋/, /Ming/, /明体/, /Ming/, /[^g]song/],
+                "楷体": [/楷/, /Kai/],
+                "仿宋": [/仿宋/, /Fangsong/],
+                "其他": [/.*/]
+            };
+            if (!font.family) {
+                Object.keys(family).some(function(f) {
+                    if (family[f].some(function(reg) {
+                        return reg.test(font.name);
+                    })) {
+                        font.family = f;
+                        return true;
+                    }
+                    return false;
+                });
+            }
+            font.index = [
+                font.name,
+                'Family:'+font.family,
+                'License:' + font.license,
+                font.commercialUse.indexOf('可') > -1 ? "可商用" : "",
+                font.embedInPDF.indexOf('可') > -1 ? "可嵌入PDF" : "",
+                font.notes
+            ].join(' ');
+            return font;
+        });
+
+        $main.append(["字体名称", "授权", "可否商用", "可否嵌入 PDF", "备注"].map(function(th) {
+            return '<th>' + th + '</th>';
+        }).join(''));
         fonts.forEach(function(f) {
             var $font = $('<tr class="font"></tr>');
+            $font[0].dataset.index = f.index;
             var $name = $('<td class="name"><a href="' + f.link + '">' + f.name + '</a></td>');
             $name.append('<i class="fa fa-info-circle" title="Copyright: ' + f.copyright + '"></i>');
             $font.append($name);
@@ -54,7 +96,6 @@ $(function() {
             $main.append($font);
             return '<tr>' + html + '</tr>';
         });
-            // <i class="fa fa-file-pdf-o"></i>
 
     });
 
