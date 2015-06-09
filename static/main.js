0e99fbba712c5547355ff1f848822d91c7738373
Zeno Zeng <zenoofzeng@gmail.com>
2015-06-10 03:42:14 +0800

    use <table>

--- static/main.js
+++ static/main.js
@@ -2,7 +2,8 @@ $(function() {
 
     var tags = {
         "Family": ["黑体", "宋体", "楷体", "仿宋", "其他"],
-        "License": ["GPL", "SIL", "APL", "CC"]
+        "License": ["GPL", "SIL", "文鼎公众授权", "CC", "Apache"],
+        "Tag": ["可商用", "可嵌入PDF"]
     };
 
     var $main = $('#main');
@@ -34,23 +35,23 @@ $(function() {
     $('#tags').on('click', '.tag', function() {
         var tag = $(this).text();
         var key = $(this).prevAll('.key').text().split(':')[0];
-        $('#filter').val($('#filter').val() + " " + key + "=" + tag);
+        $('#filter').val($('#filter').val() + " " + key + ":" + tag);
         $('#filter').trigger('input');
     });
 
 
     var fonts = $.get('src/fonts.json', function(fonts) {
         fonts.forEach(function(f) {
-            var $font = $('<div class="font"></div>');
-            var $name = $('<div class="name"><a href="' + f.link + '">' + f.name + '</a></div>');
-            $name.append('<i class="fa fa-external-link"></i>');
-            $name.append('<div class="license">' + f.license + '</div>');
+            var $font = $('<tr class="font"></tr>');
+            var $name = $('<td class="name"><a href="' + f.link + '">' + f.name + '</a></td>');
+            $name.append('<i class="fa fa-info-circle" title="Copyright: ' + f.copyright + '"></i>');
             $font.append($name);
-            $font.append('<div class="copyright">Copyright: ' + f.copyright + '</div>');
-            $main.append($font);
-            var html = [f.commercialUse, f.embedInPDF, f.link, f.name, f.notes].map(function(td) {
+            $font.append('<td class="license">' + f.license + '</td>');
+            var html = [f.commercialUse, f.embedInPDF, $(marked(f.notes)).html() || '无'].map(function(td) {
                 return '<td>' + td + '</td>';
             }).join('');
+            $font.append(html);
+            $main.append($font);
             return '<tr>' + html + '</tr>';
         });
             // <i class="fa fa-file-pdf-o"></i>
